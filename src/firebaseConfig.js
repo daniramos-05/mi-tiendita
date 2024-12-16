5e9e9674e14ca4baed671e1749b8dc41b8106ef6
Ramos Dani <dany.ramo.2005@gmail.com>
2024-12-16 14:53:46 -0300

    Variables de entorno

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB1Yq31V18YSfavKv2cJKAFlcXJYtjgfnI",
-  authDomain: "mi-tiendita-fb.firebaseapp.com",
-  projectId: "mi-tiendita-fb",
-  storageBucket: "mi-tiendita-fb.firebasestorage.app",
-  messagingSenderId: "522389095880",
-  appId: "1:522389095880:web:effc2c6e403c39acd46cfd",
+  apiKey: process.env.REACT_APP_API,
+  authDomain: process.env.REACT_APP_AUTH,
+  projectId: process.env.REACT_APP_PROJECT,
+  storageBucket: process.env.REACT_APP_STORAGE,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
